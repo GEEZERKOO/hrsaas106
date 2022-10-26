952ed039148def8bbbe85ae60565e044eb44ae16
KOO <565033820@qq.com>
2022-10-27 00:35:05 +0800

    实现token失效的主动处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 import store from '@/store'
+import router from '@/router'
 import { Message } from 'element-ui'
+import { getTimeStamp } from '@/utils/auth'
+const TimeOut = 4800 // 定义超时时间
+
 const service = axios.create({
   // 当执行dev的时候 => 。env.development => /api => 跨域代理
   baseURL: process.env.VUE_APP_BASE_API, // /dev-api  /prod-api
@@ -9,8 +13,14 @@ const service = axios.create({
 // 请求拦截器
 service.interceptors.request.use(config => {
   // config是请求的配置信息
+  // 注入token
   if (store.getters.token) {
-    // 注入token
+    if (IsCheckTimeOut()) {
+      // 如果是true就表示token过期了
+      store.dispatch('user/logout') // 登出操作
+      router.push('/login')
+      return Promise.reject(new Error('token超时了'))
+    }
     config.headers['Authorization'] = `Bearer ${store.getters.token}`
   }
   return config // 是必须要返回的
@@ -34,4 +44,12 @@ service.interceptors.response.use(response => {
   Message.error(error.message) // 提示错误信息
   return Promise.reject(error) // 返回执行错误 让当前执行链跳出成功 直接进入catch
 })
+
+// 定义检查是否超时方法
+// 当前时间 - 缓存中的时间 结果是否大于时间差
+function IsCheckTimeOut() {
+  var currentTime = Date.now() // 当前时间戳
+  var timeStamp = getTimeStamp() // 缓存的时间戳
+  return (currentTime - timeStamp) / 1000 > TimeOut
+}
 export default service
