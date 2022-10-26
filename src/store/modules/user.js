4ec1137458f235ac32736f8f6e92846a5de0298d
KOO <565033820@qq.com>
2022-10-26 16:19:23 +0800

    封装获取瀛湖资料的token并共享用户状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,9 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 // 状态
 const state = {
-  token: getToken() // 从缓存中获取数据 设置token初始状态
+  token: getToken(), // 从缓存中获取数据 设置token初始状态
+  userInfo: {} // 会在getters中引用userinfo的变量，如果设置为null，则会引起异常和报错
 }
 // 修改状态
 const mutations = {
@@ -13,6 +14,15 @@ const mutations = {
   removeToken(state) {
     state.token = null // 删除vuex的token
     removeToken() // 同步到缓存
+  },
+  setUserInfo(state, result) {
+    // 更新对象
+    state.userInfo = result
+  },
+
+  // 删除用户信息
+  removeUserInfo() {
+    state.userInfo = {}
   }
 }
 // 执行异步
@@ -21,6 +31,11 @@ const actions = {
     // 调用api接口
     const result = await login(data) // 拿到token
     context.commit('setToken', result) // 设置token
+  },
+  async getUserInfo(context) {
+    const result = await getUserInfo()
+    context.commit('setUserInfo', result) // 提交给mutations
+    return result
   }
 }
 
