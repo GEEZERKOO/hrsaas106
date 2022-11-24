cfef6b421486ef8017263347a0cb076b64e06006
KOO <565033820@qq.com>
2022-11-24 17:10:23 +0800

    删除角色功能完成

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -40,18 +40,21 @@
                 align="center"
                 label="操作"
               >
-                <el-button
-                  size="small"
-                  type="success"
-                >分配权限</el-button>
-                <el-button
-                  size="small"
-                  type="primary"
-                >编辑</el-button>
-                <el-button
-                  size="small"
-                  type="danger"
-                >删除</el-button>
+                <template slot-scope="{ row }">
+                  <el-button
+                    size="small"
+                    type="success"
+                  >分配权限</el-button>
+                  <el-button
+                    size="small"
+                    type="primary"
+                  >编辑</el-button>
+                  <el-button
+                    size="small"
+                    type="danger"
+                    @click="deleteRole(row.id)"
+                  >删除</el-button>
+                </template>
               </el-table-column>
             </el-table>
 
@@ -125,7 +128,7 @@
   </div>
 </template>
 <script>
-import { getRoleList, getCompanyInfo } from '@/api/setting'
+import { getRoleList, getCompanyInfo, deleteRole } from '@/api/setting'
 import { mapGetters } from 'vuex'
 export default {
   data() {
@@ -167,6 +170,19 @@ export default {
       // newPage是当前点击的页码
       this.page.page = newPage // 将当前页码赋值给当前的最新页码
       this.getRoleList()
+    },
+
+    // 删除角色
+    async deleteRole(id) {
+      // 提示
+      try {
+        await this.$confirm('确认删除该角色吗')
+        await deleteRole(id) // 调用删除接口
+        this.getRoleList() // 重新加载数据
+        this.$message.success('删除角色成功')
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
